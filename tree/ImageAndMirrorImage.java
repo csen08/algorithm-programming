a5d7314be8cc1cb475663addf100f8ccac144e25
Chandranath Sen <chandranath.sen@gmail.com>
2020-06-13 13:42:32 +0530

    modified

--- tree/ImageAndMirrorImage.java
+++ tree/ImageAndMirrorImage.java
@@ -4,8 +4,8 @@ public class ImageAndMirrorImage {
 	
 	/**
 	 * Different types of problems can be,
-	 * check if they are mirror image?
-	 * or convert a binary tree into mirror image
+	 * 1) check if they are mirror image?
+	 * or 2) convert a binary tree into it mirror
 	 *
 	 */
 
@@ -57,6 +57,15 @@ public class ImageAndMirrorImage {
 	
 	public static void main(String[] args) {
 		
+	    /* Construct below tree
+		        1
+		      /   \
+		     /     \
+		    2       3
+		   / \     / \
+		  4   5   6   7
+		               
+	     */
 		ImageAndMirrorImage instance = new ImageAndMirrorImage();
 		Node root = instance.new Node(1);
 		root.left = instance.new Node(2);
@@ -65,6 +74,25 @@ public class ImageAndMirrorImage {
 		root.left.right = instance.new Node(5);
 		root.right.left = instance.new Node(6);
 		root.right.right = instance.new Node(7);
+		
+		Node root2 = instance.new Node(1);
+		root2.left = instance.new Node(2);
+		root2.right = instance.new Node(3);
+		root2.left.left = instance.new Node(4);
+		root2.left.right = instance.new Node(5);
+		root2.right.left = instance.new Node(6);
+		root2.right.right = instance.new Node(7);
+		
+		System.out.println(isImage(root, root2));
+		
+		Node root3 = instance.new Node(1);
+		root3.left = instance.new Node(2);
+		root3.right = instance.new Node(3);
+		root3.left.left = instance.new Node(4);
+		root3.left.right = instance.new Node(5);
+		root3.right.left = instance.new Node(6);
+		
+		System.out.println(isImage(root, root3));
 		
 		print_in_order(root);
 		System.out.println();
