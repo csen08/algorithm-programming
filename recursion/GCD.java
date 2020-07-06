e1ae0cf26b35f4f4befb5ef185a178dbc0313cf6
Chandranath Sen <chandranath.sen@gmail.com>
2020-07-06 12:32:14 +0530

    initial commit

---
+++ recursion/GCD.java
@@ -0,0 +1,19 @@
+package programming.recursion;
+
+public class GCD {
+
+	
+	//Find greatest common divisor
+	static int gcd(int a, int b) {
+		
+		if (b==0) return a;
+		
+		else return gcd(b,a%b);
+	}
+	
+	public static void main(String[] args) {
+		
+		System.out.println(gcd(12,18));
+	}
+	
+}
