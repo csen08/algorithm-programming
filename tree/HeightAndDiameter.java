827da358185831a8873d8567deee049465ff5b02
Chandranath Sen <chandranath.sen@gmail.com>
2020-08-03 09:57:06 +0530

    changes to show one line code

--- tree/HeightAndDiameter.java
+++ tree/HeightAndDiameter.java
@@ -16,22 +16,31 @@ public class HeightAndDiameter {
 	
 	public static int height_of_a_tree(Node n) {
 		
-		if (n== null)//base condition, 
-			//in tree recursive problems, always this is base condition. 
-			return -1;
-		else
-			return (1+Math.max(height_of_a_tree(n.left), height_of_a_tree(n.right)));
+//		if (n== null)//base condition, 
+//			//in tree recursive problems, always this is base condition. 
+//			return -1; //note that, when n is null, it is -1, NOT ZERO !!!
+//		else
+//			return (1+Math.max(height_of_a_tree(n.left), height_of_a_tree(n.right)));
+//		
+		//one liner
+		return (n==null)? -1 : (1+Math.max(height_of_a_tree(n.left), height_of_a_tree(n.right)));
+		
 	}
 	
-	public static int diameter_of_a_tree(Node n, AtomicInteger diameter) {
+	public static int count_nodes(Node n) {
+		//one liner
+		return (n==null)? 0 : 1+count_nodes(n.left)+count_nodes(n.right);
+	}
+	
+	public static int diameter_of_a_tree(Node n, AtomicInteger diameter) {//we can pass here int[] or a custom IntHolder class
 		//diameter calculation is similar as height calculation
-		//with some twist
+		//with some twist :-)
 		
 		//code what we will write here is same as height calculation
 		//https://www.geeksforgeeks.org/diameter-of-a-binary-tree-in-on-a-new-method/
 		//diameter of a tree is the max value of (left_height+right_height+1) for each node
 		
-		if (n == null )//base case
+		if (n == null )//base case, note here returns 0, but for height calculation, returns -1
 			return 0;
 		
 		//calculate height of left sub tree and right sub tree
@@ -40,7 +49,7 @@ public class HeightAndDiameter {
 		
 		diameter.set(Math.max(diameter.get(), L+R+1));
 		
-		return 1+Math.max(L, R);
+		return 1+Math.max(L, R);//note that, here returns actually height of the tree
 	}
 
 	
